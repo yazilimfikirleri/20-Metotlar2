0bccbe1754d487fec28efcae830040c40e764ab0
info@yazilimfikirleri.com <info@yazilimfikirleri.com>
2020-06-02 15:07:45 +0300

    proje oluşturuldu.

---
+++ 20-Metotlar2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Metotlar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Geriye Değer Döndüren Metot
+            //Bir metot geriye değer döndürecek ise void yerine geriye dönüş tipini belirtmeliyiz. Geriye dönüş tipimiz C Sharp tarafında tanımlı olan veri tipleri (string, int, bool, double vs.) veya bizim tanımladığımız nesne tipleri olabilir. Kendi nesnelerimizi tanımlamayı ilerde inceleyeceğiz(OOP). Geriye dönüş tipi belirtildikten sonra geriye değer döndürmek için return komutu kullanılır. Return anahtar kelimesi metodumuzun yapacağı işlem sonucunda geriye döndüreceği değeri belirler. 
+
+            //string Bilgi()
+            //{
+            //    string ad;
+            //    string soyad;
+            //    Console.WriteLine("Lütfen adınızı yazın");
+            //    ad = Console.ReadLine();
+            //    Console.WriteLine("Lütfen soyadınızı yazın");
+            //    soyad = Console.ReadLine();
+
+            //    return $"adınız {ad} soyadınız: {soyad}";
+            //}
+
+            ////Metot Kullanım
+            //string kullaniciBilgisi = Bilgi();
+
+            //Console.WriteLine(kullaniciBilgisi);
+            //Console.Read();
+
+            //Dışarıdan alınan iki sayının toplamını veren metot.
+
+            int Topla(int s1, int s2)
+            {
+                int toplam = s1 + s2;
+                return toplam;
+            }
+
+            int sonuc = Topla(5, 3);
+            Console.WriteLine("Toplam sonuç= " + sonuc);
+            Console.Read();
+        }
+    }
+}
